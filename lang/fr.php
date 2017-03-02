74dc9bf5f7f8aea3b6b6243edf6a6ba7ae398b3c
jerrywham <github@ecyseo.net>
2017-03-02 23:45:24 +0100

    Update fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -5,7 +5,8 @@ $LANG = array(
 
 # config.php
 'L_HELP_SUB_DOM'		=> 'Le sous-domaine doit pointer sur le dossier data/images de PluXml (open_basedir)',
-'L_SUBDOM'				=> 'Sous-domaine',
+'L_SUBDOM'              => 'Sous-domaine',
+'L_SUBDOM_HELP'			=> 'Si vous avez la main sur votre serveur, créez un sous-domaine (par exemple http://<span style="color:red">img</span>.votredomaine.net) pointant vers le dossier contenant vos images (typiquement \'data/images\') et indiquez-le dans le champ suivant (en n\'oubliant pas le slash de fin).<br/> <b><u>Attention:</u></b> les images sur le site ne s\'afficheront que si vous avez un accès internet.',
 'L_SAVE'				=> 'Enregistrer',
 
 );
